a8d6542a8859bf63858e7454cd6423765ab0135f
laizamonique <133267385+laizamonique@users.noreply.github.com>
2023-09-13 17:18:47 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -49,10 +49,10 @@ function draw() {
     if (playerArrows[i] !== undefined) {
       playerArrows[i].display();
 
-      /* var board1Collision = Matter.SAT.collides(
+       var board1Collision = Matter.SAT.collides(
         board1.body,
         playerArrows[i].body
-      ); */
+      ); 
 
       /* var board1Collision = collides(
         board1.body,
